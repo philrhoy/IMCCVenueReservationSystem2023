b11087c0863b0c283d030aa14b335cd377ed9d2c
PuzzyDestroyery69 <lequinj7@gmail.com>
2023-11-12 23:37:53 +0800

    Updated DB

--- database/db_vrs.sql
+++ database/db_vrs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 01, 2023 at 03:09 PM
+-- Generation Time: Nov 12, 2023 at 04:36 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -55,7 +55,7 @@ INSERT INTO `number_sequence` (`id`, `page_name`, `last_number`, `CreatedDateTim
 (1, 'venues', 4, '2022-05-18 14:52:45'),
 (2, 'users', 8, '2022-05-18 14:52:58'),
 (3, 'programs', 6, '2022-07-16 13:20:28'),
-(4, 'reservations', 16, '2022-08-21 14:42:40');
+(4, 'reservations', 18, '2022-08-21 14:42:40');
 
 -- --------------------------------------------------------
 
@@ -96,6 +96,7 @@ CREATE TABLE `schedules` (
   `userID` int(11) NOT NULL,
   `venueID` int(15) DEFAULT NULL,
   `programID` int(11) NOT NULL,
+  `status` varchar(3) NOT NULL DEFAULT 'P',
   `date_start` date NOT NULL,
   `date_end` date NOT NULL,
   `time_start` varchar(50) NOT NULL,
@@ -109,8 +110,9 @@ CREATE TABLE `schedules` (
   `notified` tinyint(1) NOT NULL DEFAULT 0,
   `last_notified` date DEFAULT NULL,
   `cancelled` tinyint(1) NOT NULL DEFAULT 0,
-  `deleted` tinyint(1) NOT NULL DEFAULT 0,
-  `approved` tinyint(1) NOT NULL DEFAULT 0,
+  `deleted` tinyint(4) NOT NULL,
+  `rejectedByAdmin` int(1) NOT NULL DEFAULT 0,
+  `approvedByAdmin` int(1) NOT NULL DEFAULT 0,
   `act_form_file` varchar(255) NOT NULL,
   `letter_approve_file` varchar(255) NOT NULL,
   `notes` text NOT NULL
@@ -120,22 +122,24 @@ CREATE TABLE `schedules` (
 -- Dumping data for table `schedules`
 --
 
-INSERT INTO `schedules` (`id`, `reservationID`, `userID`, `venueID`, `programID`, `date_start`, `date_end`, `time_start`, `time_end`, `name`, `address`, `contact`, `description`, `num_participants`, `tag_color`, `notified`, `last_notified`, `cancelled`, `deleted`, `approved`, `act_form_file`, `letter_approve_file`, `notes`) VALUES
-(2, 'RES000001', 0, 1, 3, '2023-09-07', '2023-09-07', '', '', 'fgf', 'fg', '34343353535', 'er', 0, '#164dbb', 0, NULL, 0, 0, 0, '', '', ''),
-(3, 'RES000002', 0, 1, 1, '2023-09-06', '2023-09-08', '', '', 'Sample', 'sample', '21212212121', 'resr', 0, '#ee1b1b', 0, NULL, 0, 0, 0, '', '', ''),
-(4, 'RES000003', 0, 5, 6, '2023-09-07', '2023-09-07', '', '', 'Sample', 'dd', '21212212121', 're', 0, '#1aff34', 0, NULL, 0, 0, 0, '', '', ''),
-(5, 'RES000004', 0, 4, 5, '2023-09-09', '2023-09-09', '', '', 'Sample', NULL, NULL, 'ss', 0, '#ffffff', 0, NULL, 0, 0, 0, '', '', ''),
-(8, 'RES000005', 0, 2, 3, '2023-10-11', '2023-10-11', '', '', 'SSG ELECTION', NULL, NULL, 'SSG Elections for grade 6 students', 125, '#ffffff', 0, NULL, 0, 0, 0, '6527e9bc2f12c.png', '6527e9bc2f23c.png', ''),
-(9, 'RES000007', 0, 5, 2, '2023-10-13', '2023-10-13', '', '', 'PE 2 Final Performance', NULL, NULL, 'Final Requirement for P2 ', 30, '#ffffff', 0, NULL, 0, 0, 0, '6527ebe506660.png', '6527ebe506740.png', ''),
-(10, 'RES000008', 0, 4, 5, '2023-10-13', '2023-10-13', '', '', 'Sumbaganay lang sa', NULL, NULL, 'Sumbagay w/ executives', 10, '#ffffff', 0, NULL, 0, 0, 0, '6527eda766fe4.png', '6527eda7670c5.png', ''),
-(11, 'RES000009', 0, 4, 1, '2023-10-16', '2023-10-16', '', '', 'HIV Awareness Orientation', NULL, NULL, 'To raise awareness on HIV', 50, '#7d7d7d', 0, NULL, 0, 0, 0, '65282783e35c1.png', '65282783e3ee5.png', ''),
-(13, 'RES000010', 0, 1, 2, '2023-10-17', '2023-10-18', '08:00', '09:00', 'Sumbagay Dev vs HR', NULL, NULL, 'sumbagay nlng sa kay gahi kayog ulo ang HR', 25, '#ffffff', 0, NULL, 0, 0, 0, '652898b1f3b60.png', '652898b1f3cb8.png', ''),
-(14, 'RES000011', 0, 1, 5, '2023-10-01', '2023-10-01', '17:50', '17:50', 'Test 10/30/2023', NULL, NULL, 'Test objectives', 15, '#ffffff', 0, NULL, 0, 0, 0, '653f7cf006c5f.jpg', '653f7cf00747f.png', ''),
-(15, 'RES000012', 0, 4, 6, '2023-10-30', '2023-10-30', '17:56', '17:56', 'Test bug fix 1', NULL, NULL, 'Test objectives', 10, '#ffffff', 0, NULL, 0, 0, 0, '653f7de1dfb3d.png', '653f7de1dfdd5.jpg', ''),
-(16, 'RES000013', 0, 4, 6, '2023-10-01', '2023-10-02', '21:09', '21:09', 'TEST UPLOAD IMAGE', NULL, NULL, 'TEST UPLOAD IMAGE', 22, '#ffffff', 0, NULL, 0, 0, 0, '653fab140816f.jpg', '653fab140853c.png', ''),
-(17, 'RES000014', 0, 1, 2, '0000-00-00', '2023-11-30', '', '10:12', 'SK VOTE TEST', NULL, NULL, 'SK VOTE TEST', 30, '#ffffff', 0, NULL, 0, 0, 0, '653fababdd1e5.jpg', '653fababdd350.jpg', 'TEST NOTA'),
-(18, 'RES000015', 6, 4, 2, '2023-11-01', '2023-11-01', '09:05', '11:05', 'TEST USER ID FLAG', NULL, NULL, 'TEST TEST ONLY ONLY', 69, '#ffffff', 0, NULL, 0, 0, 0, '65424d578c3ed.png', '65424d578c5fd.png', ''),
-(19, 'RES000016', 6, 1, 6, '2023-11-02', '2023-11-02', '09:53', '10:54', 'TEST Image Preview', NULL, NULL, 'TEST Image Preview po', 69, '#ffffff', 0, NULL, 0, 0, 0, '654258a6d6420.png', '654258a6d652f.png', '');
+INSERT INTO `schedules` (`id`, `reservationID`, `userID`, `venueID`, `programID`, `status`, `date_start`, `date_end`, `time_start`, `time_end`, `name`, `address`, `contact`, `description`, `num_participants`, `tag_color`, `notified`, `last_notified`, `cancelled`, `deleted`, `rejectedByAdmin`, `approvedByAdmin`, `act_form_file`, `letter_approve_file`, `notes`) VALUES
+(2, 'RES000001', 4, 1, 3, 'A', '2023-09-07', '2023-09-07', '', '', 'fgf', 'fg', '34343353535', 'er', 0, '#164dbb', 0, NULL, 0, 0, 0, 0, '', '', ''),
+(3, 'RES000002', 6, 1, 1, 'R', '2023-09-06', '2023-09-08', '', '', 'SampleMODAL', 'sample', '21212212121', 'resr', 22, '#ee1b1b', 0, NULL, 0, 0, 4, 0, '', '', 'yawards'),
+(4, 'RES000003', 6, 5, 6, 'P', '2023-09-07', '2023-09-07', '', '', 'Sample', 'dd', '21212212121', 're', 0, '#1aff34', 0, NULL, 0, 0, 0, 0, '', '', ''),
+(5, 'RES000004', 4, 4, 5, 'P', '2023-09-09', '2023-09-09', '', '', 'Sample', NULL, NULL, 'ss', 0, '#ffffff', 0, NULL, 0, 0, 0, 0, '', '', ''),
+(8, 'RES000005', 4, 2, 3, 'P', '2023-10-11', '2023-10-11', '', '', 'SSG ELECTION', NULL, NULL, 'SSG Elections for grade 6 students', 125, '#ffffff', 0, NULL, 0, 0, 0, 0, '6527e9bc2f12c.png', '6527e9bc2f23c.png', ''),
+(9, 'RES000007', 6, 5, 2, 'P', '2023-10-13', '2023-10-13', '', '', 'PE 2 Final Performance', NULL, NULL, 'Final Requirement for P2 ', 30, '#ffffff', 0, NULL, 0, 0, 0, 0, '6527ebe506660.png', '6527ebe506740.png', 'test'),
+(10, 'RES000008', 4, 4, 5, 'P', '2023-10-13', '2023-10-13', '', '', 'Sumbaganay lang sa', NULL, NULL, 'Sumbagay w/ executives', 10, '#ffffff', 0, NULL, 0, 0, 0, 0, '6527eda766fe4.png', '6527eda7670c5.png', ''),
+(11, 'RES000009', 6, 4, 1, 'P', '2023-10-16', '2023-10-16', '', '', 'HIV Awareness Orientation', NULL, NULL, 'To raise awareness on HIV', 50, '#7d7d7d', 0, NULL, 0, 0, 0, 0, '65282783e35c1.png', '65282783e3ee5.png', ''),
+(13, 'RES000010', 4, 1, 2, 'P', '2023-10-17', '2023-10-18', '08:00', '09:00', 'Sumbagay Dev vs HR', NULL, NULL, 'sumbagay nlng sa kay gahi kayog ulo ang HR', 25, '#ffffff', 0, NULL, 0, 0, 0, 0, '652898b1f3b60.png', '652898b1f3cb8.png', ''),
+(14, 'RES000011', 4, 1, 5, 'P', '2023-10-01', '2023-10-01', '17:50', '17:50', 'Test 10/30/2023', NULL, NULL, 'Test objectives', 15, '#ffffff', 0, NULL, 0, 0, 0, 0, '653f7cf006c5f.jpg', '653f7cf00747f.png', ''),
+(15, 'RES000012', 4, 4, 6, 'P', '2023-10-30', '2023-10-30', '17:56', '17:56', 'Test bug fix 1', NULL, NULL, 'Test objectives', 10, '#ffffff', 0, NULL, 0, 0, 0, 0, '653f7de1dfb3d.png', '653f7de1dfdd5.jpg', ''),
+(16, 'RES000013', 6, 4, 6, 'P', '2023-10-01', '2023-10-02', '21:09', '21:09', 'TEST UPLOAD IMAGE', NULL, NULL, 'TEST UPLOAD IMAGE', 21, '#ffffff', 0, NULL, 0, 0, 0, 0, '653fab140816f.jpg', '653fab140853c.png', ''),
+(17, 'RES000014', 6, 1, 2, 'A', '0000-00-00', '2023-11-30', '', '10:12', 'SK VOTE TEST', NULL, NULL, 'SK VOTE TEST', 25, '#ffffff', 0, NULL, 0, 0, 0, 4, '653fababdd1e5.jpg', '653fababdd350.jpg', 'TEST NOTA'),
+(18, 'RES000015', 6, 4, 2, 'A', '2023-11-01', '2023-11-01', '09:05', '11:05', 'TEST USER ID FLAG', NULL, NULL, 'TEST TEST ONLY ONLY', 69, '#ffffff', 0, NULL, 0, 0, 4, 4, '65424d578c3ed.png', '65424d578c5fd.png', ''),
+(19, 'RES000016', 6, 1, 6, 'A', '2023-11-02', '2023-11-02', '09:53', '10:54', 'TEST Image Preview', NULL, NULL, 'TEST Image Preview po', 65, '#ffffff', 0, NULL, 0, 0, 4, 4, '654258a6d6420.png', '654258a6d652f.png', 'TEST NOTEs'),
+(20, 'RES000017', 4, 5, 5, 'P', '2023-11-13', '2023-11-13', '13:31', '23:36', 'TEST HIGH RESOLUTION IMAGE', NULL, NULL, '1. Temporary removed image validation\r\n2. Test out image scroll spy', 1, '#ffffff', 0, NULL, 0, 0, 0, 0, '65505554369e2.jpg', '6550555436b4b.jpg', ''),
+(21, 'RES000018', 6, 5, 2, 'P', '2023-11-16', '2023-11-16', '13:00', '14:00', 'TEST PDF IMAGE & PDF VIEWER', NULL, NULL, '1. Added new button for file preview\r\n2. Added PDF to accepted file ext.\r\n', 1, '#ffffff', 0, NULL, 0, 0, 0, 0, '655093284d456.pdf', '655093284d52f.png', '');
 
 -- --------------------------------------------------------
 
@@ -262,7 +266,7 @@ ALTER TABLE `program`
 -- AUTO_INCREMENT for table `schedules`
 --
 ALTER TABLE `schedules`
-  MODIFY `id` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `users`
